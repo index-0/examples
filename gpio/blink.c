9a8c46cfc3b4f86f58c36063df8000d94b10e199
Alfredo Pérez <index@mailbox.org>
2025-08-07 13:32:39 -0600

    gpio: add blink example

---
+++ gpio/blink.c
@@ -0,0 +1,29 @@
+/* See LICENSE file for copyright and license details. */
+
+#include <fe310/clint.h>
+#include <fe310/gpio.h>
+
+#define PINS GPIO_PIN(19) | GPIO_PIN(21) | GPIO_PIN(22)
+#define RTCFQ 32768
+
+static void
+wait(u32 s)
+{
+	u64 next = clint_get_mtime() + RTCFQ * s + 1;
+	while (clint_get_mtime() < next);
+}
+
+int
+main(void)
+{
+	gpio_cfg(GPIO_OUTPUT, PINS);
+
+	while (1) {
+		wait(1);
+		gpio_set(PINS);
+		wait(1);
+		gpio_clr(PINS);
+	}
+
+	return 0;
+}
