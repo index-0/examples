df5a450c797e389872a642fa03d93c236c6997be
Alfredo Pérez <index@mailbox.org>
2025-08-10 16:50:05 -0600

    gpio: blink: revert to mtime

--- gpio/blink.c
+++ gpio/blink.c
@@ -1,24 +1,21 @@
 /* See LICENSE file for copyright and license details. */
 
-#include <fe310/aon.h>
+#include <fe310/clint.h>
 #include <fe310/gpio.h>
 
 #define PINS GPIO_PIN(19) | GPIO_PIN(21) | GPIO_PIN(22)
 #define RTCFQ 32768
 
 static void
-wait(u32 s)
+wait(void)
 {
-	u64 next = rtc_get_count() + RTCFQ * s + 1;
-	while (rtc_get_count() < next);
+	u64 next = clint_get_mtime() + RTCFQ / 2 + 1;
+	while (clint_get_mtime() < next);
 }
 
 int
 main(void)
 {
-	/* enable rtc */
-	rtc_cfg(true, 0);
-
 	gpio_cfg(GPIO_OUTPUT, PINS);
 
 	while (1) {
