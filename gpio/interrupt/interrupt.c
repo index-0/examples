41e302cbda4a697e30c19d888b29ff37754e4e74
Alfredo Pérez <index@mailbox.org>
2025-10-20 04:58:05 -0600

    gpio: interrupt: use board-specific pin macros

--- gpio/interrupt/interrupt.c
+++ gpio/interrupt/interrupt.c
@@ -5,24 +5,26 @@
 #include <fe310/periph/prci.h>
 #include <fe310/periph/uart.h>
 
+#include "hifive1.h"
+
 static void
-gpio13_isr(void)
+pin3_isr(void)
 {
 	const char risemsg[] = "gpio13: rise\n";
 	const char fallmsg[] = "gpio13: fall\n";
 
 	/* on rise */
-	if ((gpio_get_ip(GPIO_INT_RISE) & GPIO_PIN(13)) != 0) {
+	if ((gpio_get_ip(GPIO_INT_RISE) & PIN_3) != 0) {
 		uart_putb(uart0, risemsg, sizeof(risemsg));
 		/* clear ip */
-		gpio_ip(GPIO_INT_RISE, GPIO_PIN(13));
+		gpio_ip(GPIO_INT_RISE, PIN_3);
 	}
 
 	/* on fall */
-	if ((gpio_get_ip(GPIO_INT_FALL) & GPIO_PIN(13)) != 0) {
+	if ((gpio_get_ip(GPIO_INT_FALL) & PIN_3) != 0) {
 		uart_putb(uart0, fallmsg, sizeof(fallmsg));
 		/* clear ip */
-		gpio_ip(GPIO_INT_FALL, GPIO_PIN(13));
+		gpio_ip(GPIO_INT_FALL, PIN_3);
 	}
 }
 
@@ -40,16 +42,16 @@ main(void)
 	/* for tty */
 	uart0_init();
 
-	gpio_cfg(GPIO_INPUT_PULLUP, GPIO_PIN(13));
+	gpio_cfg(GPIO_INPUT_PULLUP, PIN_3);
 
-	gpio_ie(GPIO_INT_RISE, true, GPIO_PIN(13));
-	gpio_ie(GPIO_INT_FALL, true, GPIO_PIN(13));
+	gpio_ie(GPIO_INT_RISE, true, PIN_3);
+	gpio_ie(GPIO_INT_FALL, true, PIN_3);
 
 	plic_threshold(PLIC_PRIORITY_0);
 
-	plic_hook(PLIC_IRQ_GPIO13, gpio13_isr);
-	plic_priority(PLIC_IRQ_GPIO13, PLIC_PRIORITY_1);
-	plic_ie(true, PLIC_IRQ_BIT(PLIC_IRQ_GPIO13));
+	plic_hook(PLIC_IRQ_PIN(PIN_3), pin3_isr);
+	plic_priority(PLIC_IRQ_PIN(PIN_3), PLIC_PRIORITY_1);
+	plic_ie(true, PLIC_IRQ_PIN_BIT(PIN_3));
 
 	/* wait for interrupt */
 	while (1) __asm__ volatile("wfi");
